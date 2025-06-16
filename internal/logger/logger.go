d2399a4ad24cb58288431b3ebe81c359c3e96ef8
SonyaCore <sonyacore@protonmail.com>
2025-06-16 11:52:06 +0300

    feat: implemented logger package

---
+++ internal/logger/logger.go
@@ -0,0 +1,116 @@
+package logger
+
+import (
+	"fmt"
+	"sync"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var (
+	logger  *zap.Logger
+	once    sync.Once
+	initErr error
+)
+
+func Init(level string) (*zap.Logger, error) {
+	once.Do(func() {
+		var cfg zap.Config
+
+		var logLevel zapcore.Level
+		switch level {
+		case "debug":
+			logLevel = zapcore.DebugLevel
+		case "info":
+			logLevel = zapcore.InfoLevel
+		case "warn":
+			logLevel = zapcore.WarnLevel
+		case "error":
+			logLevel = zapcore.ErrorLevel
+		default:
+			logLevel = zapcore.InfoLevel
+		}
+		cfg = zap.Config{
+			Level:       zap.NewAtomicLevelAt(logLevel),
+			Development: false,
+			Sampling: &zap.SamplingConfig{
+				Initial:    100,
+				Thereafter: 100,
+			},
+			Encoding:         "json",
+			EncoderConfig:    zap.NewProductionEncoderConfig(),
+			OutputPaths:      []string{"stderr"},
+			ErrorOutputPaths: []string{"stderr"},
+		}
+
+		logger, initErr = cfg.Build()
+		if initErr != nil {
+			initErr = fmt.Errorf("failed to initialize logger: %w", initErr)
+		}
+	})
+
+	return logger, initErr
+}
+
+func Get() (*zap.Logger, error) {
+	if logger == nil {
+		_, err := Init("info")
+		if err != nil {
+			return nil, err
+		}
+	}
+	return logger, nil
+}
+
+func MustGet() *zap.Logger {
+	l, err := Get()
+	if err != nil {
+		panic(err)
+	}
+	return l
+}
+
+func Sync() error {
+	if logger != nil {
+		return logger.Sync()
+	}
+	return nil
+}
+
+func Debug(msg string, fields ...zap.Field) {
+	if l, err := Get(); err == nil {
+		l.Debug(msg, fields...)
+	}
+}
+
+func Info(msg string, fields ...zap.Field) {
+	if l, err := Get(); err == nil {
+		l.Info(msg, fields...)
+	}
+}
+
+func Warn(msg string, fields ...zap.Field) {
+	if l, err := Get(); err == nil {
+		l.Warn(msg, fields...)
+	}
+}
+
+func Error(msg string, fields ...zap.Field) {
+	if l, err := Get(); err == nil {
+		l.Error(msg, fields...)
+	}
+}
+
+func Fatal(msg string, fields ...zap.Field) {
+	if l, err := Get(); err == nil {
+		l.Fatal(msg, fields...)
+	}
+}
+
+func With(fields ...zap.Field) *zap.Logger {
+	if l, err := Get(); err == nil {
+		return l.With(fields...)
+	}
+	return zap.NewNop()
+}
